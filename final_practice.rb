28ab40cf641ba396099d884c794522d65c6e35f5
Jon Watkins <jkwatkins85@gmail.com>
2023-02-12 15:22:45 -0700

    Add Exercise 3-4 answers

--- final_practice.rb
+++ final_practice.rb
@@ -26,12 +26,12 @@
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-def custom_greeting(name)
-    "Hello there, #{name}!"
-end
+# def custom_greeting(name)
+#     "Hello there, #{name}!"
+# end
 
-p custom_greeting("General Kenobi")
-p custom_greeting("Count Dooku")
+# p custom_greeting("General Kenobi")
+# p custom_greeting("Count Dooku")
 
 # What is the return value of your method?
 # "Hello there, #{name}!", a String Object with a string interpolation to insert the argumented value.
@@ -45,17 +45,44 @@ p custom_greeting("Count Dooku")
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+# def greet_person(first_name, middle_name, last_name)
+#     "#{first_name} #{middle_name} #{last_name}, you old dog!"
+# end
+
+# p greet_person("Obi", "Wan", "Kenobi")
+# p greet_person("Jonathan", "Kegan", "Watkins")
+
 # What is the return value of your method?
+# "#{first_name} #{middle_name} #{last_name}, you old dog!", a String Object with a string interpolation to insert the argumented values.
+
 # How many arguments did you pass your method?
+# Six, the Strings "Obi", "Wan", "Kenobi", "Jonathan", "Kegan", and "Watkins".
+
 # What data type was your argument(s)?
+# Strings.
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square(number)
+    number ** 2
+end
+
+result = square(3)
+
+p square(3)
+p square(15)
+p "#{result} is the square of 3"
+
 # What is the return value of your method?
+# The integer which is a result of multiplying the argumented value by itself.
+
 # How many arguments did you pass your method?
+# Two, the integers '3' and '15'.
+
 # What data type was your argument(s)?
+# Integers.
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
